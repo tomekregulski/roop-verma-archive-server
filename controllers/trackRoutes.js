6e34dd496733fb0096ad52f8718b6e19ea821589
Tomek Regulski <tomek.regulski@gmail.com>
2022-02-27 11:30:37 -0500

    reconnect auth middleware

--- controllers/trackRoutes.js
+++ controllers/trackRoutes.js
@@ -13,58 +13,54 @@ const {
 
 const authorization = require('../middleware/authorization');
 
-router.get(
-  '/',
-  // authorization,
-  async (req, res) => {
-    try {
-      const allTracks = await Track.findAll({
-        include: [
-          {
-            model: Tape,
-            as: 'tape',
-            include: [
-              {
-                model: Event,
-                as: 'event',
-                include: [
-                  {
-                    model: Location,
-                    as: 'location',
-                  },
-                  {
-                    model: Category,
-                    as: 'category',
-                  },
-                ],
-              },
-            ],
-          },
-          {
-            model: MediaType,
-            as: 'media_type',
-          },
-          {
-            model: Raga,
-            as: 'raga',
-          },
-          {
-            model: Artist,
-            as: 'artists',
-            through: {
-              model: TrackArtist,
+router.get('/', authorization, async (req, res) => {
+  try {
+    const allTracks = await Track.findAll({
+      include: [
+        {
+          model: Tape,
+          as: 'tape',
+          include: [
+            {
+              model: Event,
+              as: 'event',
+              include: [
+                {
+                  model: Location,
+                  as: 'location',
+                },
+                {
+                  model: Category,
+                  as: 'category',
+                },
+              ],
             },
+          ],
+        },
+        {
+          model: MediaType,
+          as: 'media_type',
+        },
+        {
+          model: Raga,
+          as: 'raga',
+        },
+        {
+          model: Artist,
+          as: 'artists',
+          through: {
+            model: TrackArtist,
           },
-        ],
-      });
-      const trackData = allTracks.map((track) => track.get({ plain: true }));
-      res.status(200).json(trackData);
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
+        },
+      ],
+    });
+    const trackData = allTracks.map((track) => track.get({ plain: true }));
+    res.status(200).json(trackData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
   }
-);
+});
 
 router.get('/public', async (req, res) => {
   try {
